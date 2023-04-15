6564387482c46d65a062ac58e4580f45bbdf2ec5
anasmustaqin <anas_22520012@stimata.ac.id>
2023-04-15 12:43:25 +0700

    Fungsi Read data

--- app/src/main/java/id/anas/crud/MainActivity.kt
+++ app/src/main/java/id/anas/crud/MainActivity.kt
@@ -1,5 +1,4 @@
 package id.anas.crud
-
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
@@ -17,11 +16,13 @@ import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import id.anas.crud.R.id.showdata
 import id.anas.crud.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
     //Membuat Kode Permintaan
     private var auth: FirebaseAuth? = null
     private val RC_SIGN_IN = 1
@@ -45,11 +46,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when (v.getId()) {
             R.id.save -> {
+
                 // Statement program untuk simpan data
+                //Mendapatkan UserID dari pengguna yang Terautentikasi
+                val getUserID = auth!!.currentUser!!.uid
+
+                //Mendapatkan Instance dari Database
+                val database = FirebaseDatabase.getInstance()
+
+                //Menyimpan Data yang diinputkan User kedalam Variable
+                val getNIM: String = nim.getText().toString()
+                val getNama: String = nama.getText().toString()
+                val getJurusan: String = jurusan.getText().toString()
+
+                // Mendapatkan Referensi dari Database
+                val getReference: DatabaseReference
+                getReference = database.reference
+
+                // Mengecek apakah ada data yang kosong
+                if (isEmpty(getNIM) || isEmpty(getNama) || isEmpty(getJurusan)) {
+                    //Jika Ada, maka akan menampilkan pesan singkan seperti berikut ini.
+                    Toast.makeText(this@MainActivity, "Data tidak boleh ada yang kosong", Toast.LENGTH_SHORT).show()
+                } else {
+                    /* Jika Tidak, maka data dapat diproses dan meyimpannya pada Database        Menyimpan data referensi pada Database berdasarkan User ID dari masing-masing Akun
+                    */
+                    getReference.child("Admin").child(getUserID).child("Mahasiswa").push()
+                        .setValue(data_mahasiswa(getNIM, getNama, getJurusan))
+                        .addOnCompleteListener(this) { //Peristiwa ini terjadi saat user berhasil menyimpan datanya kedalam Database
+                            nim.setText("")
+                            nama.setText("")
+                            jurusan.setText("")
+                            Toast.makeText(this@MainActivity, "Data Tersimpan", Toast.LENGTH_SHORT).show()
+                        }
+                }
             }
-            R.id.logout ->
-                // Statement program untuk logout/keluar
-                AuthUI.getInstance()
+
+        }
+        R.id.logout ;
+                    AuthUI.getInstance()
                     .signOut(this)
                     .addOnCompleteListener(object : OnCompleteListener<Void> {
                         override fun onComplete(p0: Task<Void>) {
@@ -59,9 +93,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                             finish()
                         }
                     })
-            R.id.showdata -> {
-            }
-        }
+        R.id.showdata ;
+            startActivity(Intent(this@MainActivity, MyListData::class.java))
+
     }
 }
 
+class MyListData {
+
+}
---
+++ app/src/test/java/id/anas/crud/MyListDataTest.kt
@@ -0,0 +1,10 @@
+package id.anas.crud
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MyListDataTest {
+
+    @org.junit.jupiter.api.Test
+    fun getDatabase() {
+    }
+}
